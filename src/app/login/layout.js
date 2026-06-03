e95586aa4a4f508e9d3ca7a0f50cb32c85ccf316
Fahad01012002 <fahad01012002@gmail.com>
2026-06-03 11:17:37 +0600

    add login functionality

---
+++ src/app/login/layout.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'Login',
+  description: 'Login to your Tutor Finder account',
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function LoginLayout({ children }) {
+  return <>{children}</>;
+}
